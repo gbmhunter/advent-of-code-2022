627b5866cadbf1942b67ff4a99b0ed71e89377d5
Geoffrey Hunter <gbmhunter@gmail.com>
2022-12-12 21:18:51 +1300

    Finished day 7.

--- src/day7.rs
+++ src/day7.rs
@@ -3,18 +3,14 @@ use std::{fs, collections::HashMap};
 pub fn run() {
     let contents = fs::read_to_string("src/day7.txt").unwrap();
 
-    let dir_sizes: HashMap<&str, u32> = HashMap::new();
+    let mut dir_sizes: HashMap<String, u32> = HashMap::new();
     let mut curr_path = String::from("/");
     for line in contents.lines() {
-        println!("Current line={:?}", line);
         if line.starts_with("$ cd") {
-            println!("Found cd command.");
             let (_, path_part) = line.split_at(5);
-            println!("path_part={}", path_part);
             if path_part == "/" {
                 curr_path = String::from("/");
             } else if path_part == ".." {
-                println!("Found .., going to parent dir.");
                 let mut bits: Vec<&str> = curr_path.split("/").collect();
                 bits = bits[0..bits.len() - 2].to_vec();
                 let parent_path = bits.join("/");
@@ -22,32 +18,55 @@ pub fn run() {
             } else {
                 curr_path = curr_path + path_part + "/";
             }
-            println!("curr_path is now: {:?}", curr_path);
         } else if line.starts_with("$ ls") {
-            println!("Found ls command.")
         } else {
             // Must be output of ls command
             if line.starts_with("dir ") {
-                println!("Found dir in ls output, ignoring.");
                 continue;
             } else {
                 // Must be a file
                 let pieces: Vec<&str> = line.split(" ").collect();
                 let file_size = pieces[0].parse::<u32>().unwrap();
-                println!("file_size={}", file_size);
-                increment_dir_sizes(&dir_sizes, &curr_path, file_size);
+                increment_dir_sizes(&mut dir_sizes, curr_path.clone(), file_size);
             }
         }
     }
+
+    let mut total_size = 0;
+    for (_, size) in &dir_sizes {
+        if *size <= 100000 {
+            total_size += size;
+        }
+    }
+    println!("part 1: total_size={}", total_size);
+
+    let total_disk_space = 70000000;
+    let required_free_space = 30000000;
+    let total_used_space = *dir_sizes.get("/").unwrap();
+    let curr_free_space = total_disk_space - total_used_space;
+
+    let min_space_we_need_to_free = required_free_space - curr_free_space;
+
+    let mut best_dir_size = total_used_space;
+    for (_, size) in &dir_sizes {
+        if *size > min_space_we_need_to_free && *size < best_dir_size {
+            best_dir_size = *size;
+        }
+    }
+
+    println!("part 2: dir_size={}", best_dir_size);
 }
 
-fn increment_dir_sizes(dirs: &HashMap<&str, u32>, dir_path: &str, size: u32) {
+fn increment_dir_sizes(dir_sizes: &mut HashMap<String, u32>, dir_path: String, size: u32) {
     let pieces: Vec<&str> = dir_path.split("/").collect();
-    println!("dir_path={:?}", dir_path);
-    println!("pieces={:?}", pieces);
-    let mut curr_path = "/";
+    let mut curr_path = "/".to_string();
     for i in 0..pieces.len() - 1 {
         let piece = pieces[i];
-        println!("piece={}", piece);
+        curr_path += piece;
+        if !curr_path.ends_with("/") {
+            curr_path += "/";
+        }
+        dir_sizes.entry(curr_path.clone()).and_modify(| old | *old += size).or_insert(size);
+        
     }
 }
