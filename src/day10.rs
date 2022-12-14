74634126e94a714a5d97ab8ddd768a524d965ba3
Geoffrey Hunter <gbmhunter@gmail.com>
2022-12-14 21:42:56 +1300

    Working on day10.

--- src/day10.rs
+++ src/day10.rs
@@ -6,19 +6,38 @@ pub fn run() {
     let mut lines = contents.lines();
 
     let mut cycle_count = 0;
+    let mut curr_command_remaining_cycles = 0;
+
     while(true) {
+        println!("Starting cycle. cycle_count={}", cycle_count);
+       
 
-        // Time to grab another instruction
-        let line = lines.next().unwrap();
-        println!("{}", line);
+        if curr_command_remaining_cycles == 0 {
+            println!("Time to read in a new command!");
+             // Time to grab another instruction
+            let mut line = "";
+            match lines.next() {
+                None => {
+                    println!("Run out of commands.");
+                    break;
+                },
+                Some(i) => line = i,
+            }
+            println!("{}", line);
+            if line == "noop" {
+                println!("Found noop");
+                curr_command_remaining_cycles = 1;
+            } else if line.starts_with("addx") {
+                println!("Found addx command");
+                let pieces: Vec<&str> = line.split(" ").collect();
+                let add_amount = pieces[1].parse::<i32>().unwrap();
+                println!("add_amount={}", add_amount);
+                curr_command_remaining_cycles = 2;
+            }
+        }
 
-        if line == "noop" {
-            println!("Found noop");
-        } else if line.starts_with("addx") {
-            println!("Found addx command");
-            let pieces: Vec<&str> = line.split(" ").collect();
-            let add_amount = pieces[1].parse::<i32>().unwrap();
-            println!("add_amount={}", add_amount);
+        if curr_command_remaining_cycles > 0 {
+            curr_command_remaining_cycles -= 1;
         }
         cycle_count += 1;
     }
