4ef9b501f631b8457b8e1f62c95737195d85e6fd
Geoffrey Hunter <gbmhunter@gmail.com>
2022-12-15 14:34:27 +1300

    Day10 part 1 done.

--- src/day10.rs
+++ src/day10.rs
@@ -5,11 +5,19 @@ pub fn run() {
     let contents = fs::read_to_string("src/day10.txt").unwrap();
     let mut lines = contents.lines();
 
-    let mut cycle_count = 0;
+    let mut cycle_count = 1; // Elves start their cycle count at 1
     let mut curr_command_remaining_cycles = 0;
+    let mut curr_command_name = "";
+    let mut curr_command_reg_value = 0;
+    let mut x_reg_value = 1;
+
+    let mut sum_of_signal_strengths = 0;
+    let signal_strength_start_count = 20;
+    let signal_strength_period = 40; // Every 40 cycles after the 20th
 
     while(true) {
-        println!("Starting cycle. cycle_count={}", cycle_count);
+        println!("Starting cycle. cycle_count={}, curr_command_name={}, curr_command_reg_value={}, curr_command_remaining_cycles={}",
+            cycle_count, curr_command_name, curr_command_reg_value, curr_command_remaining_cycles);
        
 
         if curr_command_remaining_cycles == 0 {
@@ -26,19 +34,41 @@ pub fn run() {
             println!("{}", line);
             if line == "noop" {
                 println!("Found noop");
+                curr_command_name = line;
                 curr_command_remaining_cycles = 1;
             } else if line.starts_with("addx") {
                 println!("Found addx command");
                 let pieces: Vec<&str> = line.split(" ").collect();
                 let add_amount = pieces[1].parse::<i32>().unwrap();
                 println!("add_amount={}", add_amount);
+                curr_command_name = pieces[0];
+                curr_command_reg_value = add_amount;
                 curr_command_remaining_cycles = 2;
             }
         }
 
+        // During cycle
+        if (cycle_count - signal_strength_start_count) % signal_strength_period == 0 {
+            println!("Time to calculate signal strength.");
+            let signal_strength = cycle_count * x_reg_value;
+            println!("signal_strength={}", signal_strength);
+            sum_of_signal_strengths += signal_strength;
+        }
+
+
+
         if curr_command_remaining_cycles > 0 {
             curr_command_remaining_cycles -= 1;
         }
+
+        if curr_command_remaining_cycles == 0 {
+            // Command has reached the end of it's running time
+            if curr_command_name == "addx" {
+                x_reg_value += curr_command_reg_value;
+                println!("Update value of reg X to {} (end of cycle {})", x_reg_value, cycle_count);
+            }
+        }
         cycle_count += 1;
     }
+    println!("Sum of signal strengths = {}", sum_of_signal_strengths);
 }
