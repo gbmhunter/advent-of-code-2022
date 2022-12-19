d78c4998b3eb03943336b3d4b397db9685fe87d1
Geoffrey Hunter <gbmhunter@gmail.com>
2022-12-19 22:04:14 +1300

    More day14.

--- src/day14.rs
+++ src/day14.rs
@@ -1,9 +1,9 @@
 use std::fs;
 
 #[derive(Debug)]
-struct Coord(u32, u32);
+struct Coord(usize, usize);
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq)]
 enum PixelTypes {
     Nothing,
     Rock,
@@ -11,11 +11,13 @@ enum PixelTypes {
     SourceOfSand,
 }
 
+const SAND_POINT: Coord = Coord(500, 0);
+
 pub fn run() {
     println!("day14");
     let contents = fs::read_to_string("src/day14.txt").unwrap();
 
-    let sand_point = Coord(500, 0);
+    
 
     // Instantiate 2D map to hold rock and sand locations. We could scale (500, 0) to be (0, 0)
     // to save on a bit of memory but not really worth it
@@ -29,7 +31,7 @@ pub fn run() {
         let rock_coords = pieces.iter().
             map(| piece | {
                 let numbers = piece.split(",").collect::<Vec<_>>();
-                return Coord(numbers[0].parse::<u32>().unwrap(), numbers[1].parse::<u32>().unwrap())
+                return Coord(numbers[0].parse::<usize>().unwrap(), numbers[1].parse::<usize>().unwrap())
             }).collect::<Vec<_>>();
         println!("{:?}", rock_coords);
         for i in 0..rock_coords.len() - 1 {
@@ -55,6 +57,23 @@ fn draw_line(map: &mut Vec<Vec<PixelTypes>>, from: &Coord, to: &Coord) {
     for x in x_range {
         for y in y_range.clone() {
             map[x as usize][y as usize] = PixelTypes::Rock;
+        }
+    }
+}
+
+fn produce_sand(map: &mut Vec<Vec<PixelTypes>>) {
+    let mut sand_location = SAND_POINT;
+    loop {
+        if map[sand_location.0][sand_location.1 + 1] == PixelTypes::Nothing {
+            sand_location = Coord(sand_location.0, sand_location.1 + 1);
+        } else if map[sand_location.0 - 1][sand_location.1 + 1] == PixelTypes::Nothing {
+            sand_location = Coord(sand_location.0 - 1, sand_location.1 + 1);
+        } else if map[sand_location.0 + 1][sand_location.1 + 1] == PixelTypes::Nothing {
+            sand_location = Coord(sand_location.0 + 1, sand_location.1 + 1);
+        } else {
+            println!("Sand has come to a rest!");
+            map[sand_location.0][sand_location.1] = PixelTypes::Sand;
+            return;
         }
     }
 }
