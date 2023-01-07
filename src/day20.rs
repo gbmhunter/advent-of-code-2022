6b498f0261673cf76239ceccc40738674ee7fd1e
Geoffrey Hunter <gbmhunter@gmail.com>
2023-01-08 11:22:17 +1300

    Finished day 20 part 2.

--- src/day20.rs
+++ src/day20.rs
@@ -25,22 +25,55 @@ pub fn run() {
         return line.parse::<i64>().unwrap()
     }).collect();
 
+    // PART 1
+
+    let sum_of_grove_coordinates_part1 = mix_and_find_sum_of_coords(&unmod_input, 1);
+
+    println!("part 1: sum of grove coordinates = {}", sum_of_grove_coordinates_part1);
+    if use_example {
+        assert!(sum_of_grove_coordinates_part1 == 3);
+    } else {
+        assert!(sum_of_grove_coordinates_part1 == 10831);
+    }
+
+    // PART 2
+
+    let decryption_key: i64 = 811589153;
+
+    let unmod_input_with_key: Vec<_> = unmod_input
+        .iter()
+        .map(|&num| {
+            num*decryption_key
+        }).collect();
+
+    let sum_of_grove_coordinates_part2 = mix_and_find_sum_of_coords(&unmod_input_with_key, 10);
+
+    println!("part 2: sum of grove coordinates = {}", sum_of_grove_coordinates_part2);
+    if use_example {
+        assert!(sum_of_grove_coordinates_part2 == 1623178306);
+    } else {
+        assert!(sum_of_grove_coordinates_part2 == 6420481789383);
+    }
+
+}
+
+fn mix_and_find_sum_of_coords(unmod_input: &Vec<i64>, num_times: i64) -> i64 {
     let mut mixed_list: Vec<_> = (0..unmod_input.len()).collect();
 
-    println!("{:?}", unmod_input);
+    for _ in 0..num_times {
+        for (unmod_input_idx, unmod_input) in unmod_input.iter().enumerate() {
+            // Find out where this unmod value is in the mixed list
+            let mixed_list_idx = mixed_list.iter().position(|&mixed_list_val| {
+                return mixed_list_val == unmod_input_idx; 
+            }).unwrap();
 
-    for (unmod_input_idx, unmod_input) in unmod_input.iter().enumerate() {
-        // Find out where this unmod value is in the mixed list
-        let mixed_list_idx = mixed_list.iter().position(|&mixed_list_val| {
-            return mixed_list_val == unmod_input_idx; 
-        }).unwrap();
+            // Remove from mixed list
+            mixed_list.remove(mixed_list_idx);
 
-        // Remove from mixed list
-        mixed_list.remove(mixed_list_idx);
-
-        // Insert back into list at moved location, wrapping as needed
-        let new_mixed_list_idx = (mixed_list_idx as i64 + unmod_input).rem_euclid(mixed_list.len() as i64) as usize;
-        mixed_list.insert(new_mixed_list_idx, unmod_input_idx);
+            // Insert back into list at moved location, wrapping as needed
+            let new_mixed_list_idx = (mixed_list_idx as i64 + unmod_input).rem_euclid(mixed_list.len() as i64) as usize;
+            mixed_list.insert(new_mixed_list_idx, unmod_input_idx);
+        }
     }
 
     // Now we have to find the 0, there should be 1 of these in the original numbers
@@ -59,16 +92,8 @@ pub fn run() {
 
     let sum_of_grove_coordinates: i64 = [1000, 2000, 3000].iter().map(|num| {
         let unmod_input_idx = mixed_list[(mixed_list_0_idx + num) % mixed_list.len()];
-        let number = unmod_input[unmod_input_idx];
-        println!("unmod_input_idx = {}, num = {}", unmod_input_idx, number);
-        number
+        return unmod_input[unmod_input_idx];
     }).sum();
 
-    println!("part 1: sum of grove coordinates = {}", sum_of_grove_coordinates);
-    if use_example {
-        assert!(sum_of_grove_coordinates == 3);
-    } else {
-        assert!(sum_of_grove_coordinates == 10831);
-    }
-
+    return sum_of_grove_coordinates;
 }
